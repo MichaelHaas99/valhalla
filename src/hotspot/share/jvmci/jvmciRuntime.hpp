1a9bf07a12117675c58667d5a1a1755310c67ae2
MichaelHaas99 <michael.haas0612@gmail.com>
2024-12-12 10:55:34 +0100

    support a runtime call for flat arrays with an unknown type

--- src/hotspot/share/jvmci/jvmciRuntime.hpp
+++ src/hotspot/share/jvmci/jvmciRuntime.hpp
@@ -528,6 +528,8 @@ class JVMCIRuntime: public CHeapObj<mtJVMCI> {
   static jboolean object_notify(JavaThread* current, oopDesc* obj);
   static jboolean object_notifyAll(JavaThread* current, oopDesc* obj);
   static jboolean substitutability_check(JavaThread* current, oopDesc* left, oopDesc* right);
+  static void load_unknown_inline(JavaThread* thread, flatArrayOopDesc* array, int index);
+  static void store_unknown_inline(instanceOopDesc* buffer, flatArrayOopDesc* array, int index);
 
   static void vm_error(JavaThread* current, jlong where, jlong format, jlong value);
   static oopDesc* load_and_clear_exception(JavaThread* thread);
