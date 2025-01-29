b2502a90844d28c24fb0f9194dfedfefb66f1145
MichaelHaas99 <michael.haas0612@gmail.com>
2025-01-29 19:15:22 +0100

    Undo changes made because JIT method was not attached in by Graal

--- src/hotspot/share/interpreter/linkResolver.hpp
+++ src/hotspot/share/interpreter/linkResolver.hpp
@@ -272,9 +272,9 @@ class LinkResolver: AllStatic {
   static void resolve_invokespecial  (CallInfo& result, Handle recv,
                                       const constantPoolHandle& pool, int index, TRAPS);
   static void resolve_invokevirtual  (CallInfo& result, Handle recv,
-                                      const constantPoolHandle& pool, int index, bool check_null_or_abstract, TRAPS);
+                                      const constantPoolHandle& pool, int index, TRAPS);
   static void resolve_invokeinterface(CallInfo& result, Handle recv,
-                                      const constantPoolHandle& pool, int index, bool check_null_or_abstract, TRAPS);
+                                      const constantPoolHandle& pool, int index, TRAPS);
   static void resolve_invokedynamic  (CallInfo& result,
                                       const constantPoolHandle& pool, int index, TRAPS);
   static void resolve_invokehandle   (CallInfo& result,
